7df9ee3d59350d463190f9b3b9aea991d9cd2853
lamalajaji <Lama.Mohammed.elq.js@tuwaiq.edu.sa>
2021-12-11 03:54:41 +0300

    posts

--- src/components/Posts/index.js
+++ src/components/Posts/index.js
@@ -1,18 +1,59 @@
-import React, { useState , useEffect} from "react";
-import axios from "axios";
-import "./style.css";
+// import React, { useState, useEffect } from "react";
+// import { Link } from 'react-router-dom'
+// import { useDispatch, useSelector } from "react-redux";
+// import axios from "axios";
+// import "./style.css";
 
+// const Posts = () => {
+//   let [posts, setPosts] = useState([]);
 
-const Posts = () => {
-    const [posts, setPosts] = useState("");
+// const state = useSelector((state) => {
+//   return {
+//     Login: state.Login,
+//   };
+// });
 
+//   useEffect(()=>{
+//       getPosts();
+//       console.log(posts);
+//   },[])
+//   const getPosts = async () => {
+//     try {
+//       const res = await axios.get(`${process.env.REACT_APP_BASE_URL}/posts`, {
+//         headers: {
+//           Authorization: `Bearer ${state.Login.token}`,
+//         },
+//       }); setPosts(res.data)
+//     } catch (error) {
+//       console.log(error);
+//     }
+//   };
 
-    
-    return (
-        <div>
-           posts 
-        </div>
-    )
-}
+//   return 
 
-export default Posts;
+//   <div>
+//       {!state.Login.token? (
+       
+//           <div className="wrapper"> 
+//           <h1> You Have to  <Link to="signup"> Sign Up </Link> or <Link to="/"> Login </Link> </h1>
+         
+// ): (
+
+// )
+
+//           </div>
+//       )}
+//       <div className="post">
+//           {posts.map((post)=>{
+              
+//  <h1>{post.title}</h1>
+
+//           })}
+
+         
+//       </div>
+//   </div>
+  
+// };
+
+// export default Posts;
